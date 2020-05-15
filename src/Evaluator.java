64fd3b40ce2b1ca5ebe5f19d19415d567d5b2081
willcohen2000 <willcohen2000@gmail.com>
2020-05-15 09:02:15 -0400

    Read user inputted expression

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Evaluator {
+
+    public static void main(String[] args) {
+        while (true) {
+            Scanner read = new Scanner(System.in);
+            System.out.println("\nInput your expression");
+            System.out.println("Answer: " + evaluate(read.next()));
+        }
+    }
+
+    public static double evaluate(String expression) {
+        System.out.println("Your expression: " + expression);
+        return 0;
+    }
 
 }
